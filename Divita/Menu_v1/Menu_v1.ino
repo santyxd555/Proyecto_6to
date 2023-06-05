d9587831f6dd9349f56a9c7739d25474270add89
FrancoDivita <135255881+FrancoDivita@users.noreply.github.com>
2023-06-05 18:19:12 -0300

    Add files via upload

--- Divita/Menu_v1/Menu_v1.ino
+++ Divita/Menu_v1/Menu_v1.ino
@@ -22,10 +22,22 @@ void setup() {
   
 }
 
-void menu()
-{
-  switch(Menu){
-  case 0:
+void cuadrados(){
+
+   switch(Cuad){
+     case 0:
+    display.drawRect(2, 18, 124, 20, WHITE);
+    break;
+
+    case 1:
+    display.drawRect(2, 0, 124, 20, WHITE); 
+    break;
+   }
+    display.display();
+}
+  
+
+void loop(){
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
@@ -33,68 +45,13 @@ void menu()
   display.println("MANUAL");
   display.setCursor(5,20);
   display.println("AUTOMATICO");
-  display.display();
-  break;
-
-  case 1:
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(25,2);
-  display.println("¿APAGAR?");
-  display.setCursor(5,20);
-  display.println("SI    NO");
-  display.display();
-  break;
-
-  case 2:
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(25,2);
-  display.println(" MODO");
-  display.setCursor(5,20);
-  display.println("MANUAL");
-  display.display();
-  break;
-
-  case 3:
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(25,2);
-  display.println("PANORAMICO");
-  display.setCursor(5,20);
-  display.println(" MOVIL");
-  display.display();
-  break;
-}
-}
-
-void cuadrados(){
-   switch(Cuad){
-     
-     case 0:
-    display.clearDisplay();
-    display.drawRect(2, 18, 124, 20, WHITE);  
-    break;
-
-    case 1:
-    display.clearDisplay();
-    display.drawRect(2, 0, 124, 20, WHITE); 
-    break;
-   }
-}
-
-void loop(){
-
+  
+ 
 X = analogRead(A0); 
 Y = analogRead(A1);
-mapY = map(Y, 0, 1023, 0, 7); // izquierda-derecha [1-2]
-mapX = map(X, 0, 1023, 7, 0); // arriba - abajo    [3-4] 
+mapY = map(Y, 0, 1023, 7, 0); // izquierda-derecha [1-2]
+mapX = map(X, 0, 1023, 0, 7); // arriba - abajo    [3-4] 
 
-menu();
-Menu=0; 
 cuadrados();
 if(mapY == 7){Cuad=0;}
 else if(mapY == 0){Cuad=1;}
