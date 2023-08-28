f5537cd3dcf75d57b8b31552efc6d34a5ce61f48
santyxd555 <118922750+santyxd555@users.noreply.github.com>
2023-08-28 17:34:47 -0300

    Create MPU6050_V.8_casi_definitivo.ino

---
+++ Santiago/Servo_MPU6050/MPU6050_V.8_casi_definitivo.ino
@@ -0,0 +1,47 @@
+#include <Wire.h>       
+#include <I2Cdev.h>  
+#include <MPU6050.h>  
+#include <Servo.h>
+
+Servo servo;
+#define servopin 9
+
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+struct MyData {
+  byte X;
+  byte Y;
+  byte Z;
+};
+
+MyData data;
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.initialize();
+  //pinMode(LED_BUILTIN, OUTPUT);
+
+  servo.attach(servopin);
+}
+
+void loop()
+{
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  data.X = map(ax, -17000, 17000, 0, 180); // X axis data
+  data.Y = map(ay, -17000, 17000, 0, 180); 
+  data.Z = map(az, -17000, 17000, 0, 180);  // Y axis data
+  delay(50);
+
+  Serial.print("  X = ");
+  Serial.print(data.X);
+  Serial.print("  Y = ");
+  Serial.print(data.Y);
+  Serial.print("  Z  = ");
+  Serial.println(data.Z);
+
+  servo.write(data.Z);
+}
